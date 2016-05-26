5dd948b674a8bc6c625b65cd0195dd230646a5de
Marvin Teichmann <marvin.teichmann@googlemail.com>
2016-05-26 11:45:07 -0400

    Add more debug options
    Conflicts:
    	test_vgg16.py
    	test_vgg16_fcn.py
    	vgg16_fcn.py

--- vgg16_fcn.py
+++ vgg16_fcn.py
@@ -36,46 +36,63 @@ class Vgg16FCN:
             green - VGG_MEAN[1],
             red - VGG_MEAN[2],
         ])
+
+        bgr=tf.Print(bgr, [tf.shape(bgr)], message='Shape of bgr')
         # assert bgr.get_shape().as_list()[1:] == [224, 224, 3]
 
         self.conv1_1 = self._conv_layer(bgr, "conv1_1")
         self.conv1_2 = self._conv_layer(self.conv1_1, "conv1_2")
         self.pool1 = self._max_pool(self.conv1_2, 'pool1')
 
+        self.pool1=tf.Print(self.pool1, [tf.shape(self.pool1)], message='Shape of pool1')
+
         self.conv2_1 = self._conv_layer(self.pool1, "conv2_1")
         self.conv2_2 = self._conv_layer(self.conv2_1, "conv2_2")
         self.pool2 = self._max_pool(self.conv2_2, 'pool2')
+
+        self.pool2=tf.Print(self.pool2, [tf.shape(self.pool2)], message='Shape of pool2')
 
         self.conv3_1 = self._conv_layer(self.pool2, "conv3_1")
         self.conv3_2 = self._conv_layer(self.conv3_1, "conv3_2")
         self.conv3_2 = self._conv_layer(self.conv3_2, "conv3_3")
         self.pool3 = self._max_pool(self.conv3_2, 'pool3')
 
+        self.pool3=tf.Print(self.pool3, [tf.shape(self.pool3)], message='Shape of pool3')
+
         self.conv4_1 = self._conv_layer(self.pool3, "conv4_1")
         self.conv4_2 = self._conv_layer(self.conv4_1, "conv4_2")
         self.conv4_3 = self._conv_layer(self.conv4_2, "conv4_3")
         self.pool4 = self._max_pool(self.conv4_3, 'pool4')
+
+        self.pool4=tf.Print(self.pool4, [tf.shape(self.pool4)], message='Shape of pool4')
 
         self.conv5_1 = self._conv_layer(self.pool4, "conv5_1")
         self.conv5_2 = self._conv_layer(self.conv5_1, "conv5_2")
         self.conv5_3 = self._conv_layer(self.conv5_2, "conv5_3")
         self.pool5 = self._max_pool(self.conv5_3, 'pool5')
 
+        self.pool5=tf.Print(self.pool5, [tf.shape(self.pool5)], message='Shape of pool5')
+
         self.fc6 = self._fc_layer(self.pool5, "fc6")
         # assert self.fc6.get_shape().as_list()[1:] == [4096]
-
         self.relu6 = tf.nn.relu(self.fc6)
         if train:
             self.relu6 = tf.nn.dropout(self.relu6, 0.5)
+
+        self.fc6=tf.Print(self.fc6, [tf.shape(self.fc6)], message='Shape of fc6')
+
 
         self.fc7 = self._fc_layer(self.relu6, "fc7")
         self.relu7 = tf.nn.relu(self.fc7)
         if train:
             self.relu7 = tf.nn.dropout(self.relu7, 0.5)
 
+        self.fc7=tf.Print(self.fc7, [tf.shape(self.fc7)], message='Shape of fc7')
+
         self.fc8 = self._fc_layer(self.relu7, "fc8")
 
         self.pred = tf.argmax(self.fc8, dimension=3)
+        self.fc8=tf.Print(self.fc8, [tf.shape(self.fc8)], message='Shape of fc8')
 
         self.up = self._upscore_layer(self.fc8, shape=tf.shape(bgr),
                                       num_classes=1000,
@@ -117,6 +134,9 @@ class Vgg16FCN:
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='VALID')
             conv_biases = self.get_bias(name)
             bias = tf.nn.bias_add(conv, conv_biases)
+
+            message = 'Shape of %s' % name
+            # bias=tf.Print(bias, [tf.shape(bias)], message=message)
 
             return bias
 
